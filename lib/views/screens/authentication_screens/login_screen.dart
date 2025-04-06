e3acd751b632e1446049d6d9863faab14e0b3ba8
yonas790 <yoni2752@gmail.com>
2025-04-06 23:04:58 +0300

    create text form field for login screen

--- lib/views/screens/authentication_screens/login_screen.dart
+++ lib/views/screens/authentication_screens/login_screen.dart
@@ -7,31 +7,91 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Loing Your Account",
-              style: GoogleFonts.lato(
-                color: Color(0xFF0d120E),
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0.2,
-                fontSize: 23,
+      backgroundColor: Colors.white.withAlpha(200),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Loing Your Account",
+                style: GoogleFonts.lato(
+                  color: Color(0xFF0d120E),
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.2,
+                  fontSize: 23,
+                ),
               ),
-            ),
-            Text(
-              "To Explore the world exclusives",
-              style: GoogleFonts.getFont(
-                'Lato',
-                color: Color(0xFF0d120E),
-                fontSize: 14,
-                letterSpacing: 0.2,
+              Text(
+                "To Explore the world exclusives",
+                style: GoogleFonts.getFont(
+                  'Lato',
+                  color: Color(0xFF0d120E),
+                  fontSize: 14,
+                  letterSpacing: 0.2,
+                ),
               ),
-            ),
-            Image.asset('assets/images/Illustration.png',
-                width: 200, height: 200),
-          ],
+              Image.asset('assets/images/Illustration.png',
+                  width: 200, height: 200),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('Email',
+                    style: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.2,
+                    )),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'enter your email',
+                    labelStyle: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontSize: 14,
+                      letterSpacing: 0.1,
+                    ),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset('assets/icons/email.png',
+                          width: 20, height: 20),
+                    )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  labelText: 'enter your Password',
+                  labelStyle: GoogleFonts.getFont(
+                    'Nunito Sans',
+                    fontSize: 14,
+                    letterSpacing: 0.1,
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset('assets/icons/password.png',
+                        width: 20, height: 20),
+                  ),
+                  suffixIcon: Icon(Icons.visibility),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
