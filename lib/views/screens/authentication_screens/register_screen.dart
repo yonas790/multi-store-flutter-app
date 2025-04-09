d35c6b33e5b0fa6466fbc3f3bd80046d0638ecb2
yonas790 <yoni2752@gmail.com>
2025-04-09 22:55:29 +0300

    create register screen

--- lib/views/screens/authentication_screens/register_screen.dart
+++ lib/views/screens/authentication_screens/register_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:store_app/views/screens/authentication_screens/register_screen.dart';
+import 'package:store_app/views/screens/authentication_screens/login_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class LoginScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Loing Your Account",
+                "Create Your Account",
                 style: GoogleFonts.lato(
                   color: Color(0xFF0d120E),
                   fontWeight: FontWeight.bold,
@@ -62,6 +62,39 @@ class LoginScreen extends StatelessWidget {
                     prefixIcon: Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: Image.asset('assets/icons/email.png',
+                          width: 20, height: 20),
+                    )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('User Name',
+                    style: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.2,
+                    )),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'enter your full name',
+                    labelStyle: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontSize: 14,
+                      letterSpacing: 0.1,
+                    ),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset('assets/icons/user.jpeg',
                           width: 20, height: 20),
                     )),
               ),
@@ -184,7 +217,7 @@ class LoginScreen extends StatelessWidget {
                     ),
                     Center(
                       child: Text(
-                        'Sing in',
+                        'Sing Up',
                         style: GoogleFonts.getFont(
                           'Nunito Sans',
                           color: Colors.white,
@@ -204,7 +237,7 @@ class LoginScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    'Need an account?',
+                    'Already have an account?',
                     style: GoogleFonts.roboto(
                       fontWeight: FontWeight.bold,
                       letterSpacing: 1,
@@ -215,11 +248,11 @@ class LoginScreen extends StatelessWidget {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => RegisterScreen(),
+                            builder: (context) => LoginScreen(),
                           ));
                     },
                     child: Text(
-                      'Sign up',
+                      'Sign In',
                       style: GoogleFonts.roboto(
                         color: Color(0xFF103DE5),
                         fontWeight: FontWeight.bold,
